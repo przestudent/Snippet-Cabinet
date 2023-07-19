b1706982dd1e67e5147707ae85d12ecdb1629b9e
Przemek Pałac <przemekpalac@student.agh.edu.pl>
2023-07-19 22:09:24 +0200

    Fixed public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { authMiddleware } from '@clerk/nextjs';
 
-export default authMiddleware({ publicRoutes: ['/', '/snippet'] });
+export default authMiddleware({ publicRoutes: ['/', '/snippet/:snippetId'] });
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
